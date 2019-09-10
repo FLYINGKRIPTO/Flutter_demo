61ecec5c680c1de27d513c015b8af4fb316f724a
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-10 11:46:20 +0530

    Using Row Expanded Text

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
     return Center(
-      child:   Container(
-        alignment: Alignment.center,
-        color: Colors.amber,
-       // width: 200.0,
-       // height: 100.0,
-       // margin: EdgeInsets.only(left: 15.0,top: 30.0),
-       // padding: EdgeInsets.all(22.0),
-        child: Text("Flight Takes",textDirection: TextDirection.ltr,
-         style: TextStyle(
-            decoration: TextDecoration.none,
-            fontSize: 40.0,
-            fontFamily: 'Gayathri',
-            fontWeight:  FontWeight.w700
-        ),
-        ),
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.amber,
+            // width: 200.0,
+            // height: 100.0,
+            // margin: EdgeInsets.only(left: 15.0,top: 30.0),
+            // padding: EdgeInsets.all(22.0),
+            child: Row(
+              children: <Widget>[
+                //1st CHILD
+                Text(
+                  "Spicejet ",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 40.0,
+                    fontFamily: 'Gayathri',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.deepPurple,
+                  ),
+                ),
 
+                //2nd Child
+                Expanded(child: Text(
+                  "Bengaluru to Mumbai",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 40.0,
+                    fontFamily: 'Gayathri',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.deepPurple,)
+                  )),
 
-      )
-    ) ;
+              ],
+            )));
   }
 }
