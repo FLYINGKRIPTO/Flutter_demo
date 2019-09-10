2503361e7f2de5dc28f5c01a001ef0642ce9982f
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-10 12:56:14 +0530

    ListView Basic

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,38 @@ import 'package:project_one/app_screens/home.dart';
 void main(){
   runApp(MaterialApp(
     title: "Exploring UI Widgets",
-    home : Home(),
+    home : Scaffold(
+      body: getListView(),
+    )
   ));
+}
+Widget getListView(){
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View"),
+        trailing: Icon(Icons.wb_sunny),
+
+      ),
+
+      ListTile(
+        leading: Icon(Icons.assistant),
+        title: Text("Assistant"),
+        //subtitle: Text("Beautiful View"),
+        //trailing: Icon(Icons.wb_sunny),
+
+      ),
+
+      ListTile(
+        //leading: Icon(Icons.landscape),
+        title: Text("Rahul Subramaminan"),
+        subtitle: Text("Standup artist"),
+        trailing: Icon(Icons.art_track),
+
+      )
+    ],
+  );
+  return listView;
 }
